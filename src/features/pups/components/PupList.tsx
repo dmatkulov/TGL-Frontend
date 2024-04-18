093645d22a0049a423f374ff7ff14bfe5aedb05c
zanylbajguttieva <janabaiguttieva@gmail.com>
2024-04-18 16:30:29 +0600

    Added PupItem.tsx, PupList.tsx in Addresses.tsx and updated typePup.d.ts

---
+++ src/features/pups/components/PupList.tsx
@@ -0,0 +1,32 @@
+import {selectPups, selectPupsLoading} from '../pupsSlice';
+import {useAppDispatch, useAppSelector} from '../../../app/hooks';
+import {useEffect} from 'react';
+import {fetchPups} from '../pupsThunks';
+import {CircularProgress, Stack} from '@mui/material';
+import PupItem from './PupItem';
+
+const PupList = () => {
+  const loading = useAppSelector(selectPupsLoading);
+  const dispatch = useAppDispatch();
+  const pups  = useAppSelector(selectPups);
+
+  useEffect(() => {
+    dispatch(fetchPups());
+  }, [dispatch]);
+
+  return (
+    <>
+      {loading && <CircularProgress />}
+      <Stack>
+        {pups.map((pup) => (
+          <PupItem
+            key={pup._id}
+            pupItem={pup}
+          />
+        ))}
+      </Stack>
+    </>
+  );
+};
+
+export default PupList;

fad6e7229b3da9cc197ef31adec214a9be764e8b
azatbek <sadykowazatbek@gmail.com>
2024-04-18 21:54:27 +0600

    TGL-50.feat

---
+++ src/features/orders/components/UserOrdersTracking.tsx
@@ -0,0 +1,78 @@
+import {Box, Button, Card, CardContent, Grid, TextField, Typography} from '@mui/material';
+
+const UserOrdersTracking = () => {
+  const cards = Array.from({ length: 3 });
+
+  return (
+    <>
+      <Box component="form">
+        <TextField
+          required
+          name="search"
+          label="поиск по трек номеру"
+          sx={{ width: "500px", mt: 1 }}
+        />
+        <Button
+          type="submit"
+          sx={{ ml: 2, mt: 2 }}
+          variant="contained"
+        >
+          Поиск
+        </Button>
+      </Box>
+
+      {cards.map((_) => (
+        <Card sx={{ mt: 2, mb: 2 }}>
+          <CardContent>
+            <Grid container spacing={2}>
+              <Grid container spacing={6} sx={{ ml: 1, borderBottom: '1px solid #000' }}>
+                <Grid item>
+                  <Typography>
+                    Объем:
+                  </Typography>
+                </Grid>
+                <Grid item>
+                  <Typography>
+                    вес - 2кг,
+                  </Typography>
+                </Grid>
+                <Grid item>
+                  <Typography>
+                    ширина - 9м,
+                  </Typography>
+                </Grid>
+                <Grid item>
+                  <Typography>
+                    высота - 200м,
+                  </Typography>
+                </Grid>
+                <Grid item>
+                  <Typography>
+                    длинна - 2км
+                  </Typography>
+                </Grid>
+              </Grid>
+              <Grid item xs={7}>
+                <Typography gutterBottom>
+                  Трекинговый номер: sdlwpd209093so
+                </Typography>
+              </Grid>
+              <Grid item xs={7}>
+                <Typography gutterBottom>
+                  статус: КР_ПРИБЫЛО
+                </Typography>
+              </Grid>
+              <Grid item xs={7}>
+                <Typography gutterBottom>
+                  цена: 200 сом
+                </Typography>
+              </Grid>
+            </Grid>
+          </CardContent>
+        </Card>
+      ))}
+    </>
+  );
+};
+
+export default UserOrdersTracking;

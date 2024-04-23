49cf80cad4644dd1a58e7a105569c14e90aaa598
Dilshad Matkulov <dilshadmatkulov@.gmail.com>
2024-04-23 22:31:26 +0600

    TGL-67.feat: created theme, added component for social media

---
+++ src/theme.ts
@@ -0,0 +1,14 @@
+import { createTheme } from '@mui/material/styles';
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      light: '#AECCCD',
+      main: '#5F9EA0',
+      dark: '#219195',
+      contrastText: '#fff',
+    },
+  },
+});
+
+export default theme;

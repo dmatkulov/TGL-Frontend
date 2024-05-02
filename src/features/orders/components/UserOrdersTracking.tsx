4c5bd7d306b6ceeedd7b9ed6169acdc825082fb8
Sergeev Mikhail <sergeev-michael@mail.ru>
2024-05-02 10:35:01 +0600

    "TGL-90(update): Remove hardcoded code on UserOrdersTracking component"

--- src/features/orders/components/UserOrdersTracking.tsx
+++ src/features/orders/components/UserOrdersTracking.tsx
@@ -1,8 +1,6 @@
 import {
   Box, Button,
-  Card,
-  CardContent,
-  Grid, TextField, Typography,
+  TextField, Typography,
   useMediaQuery,
 } from '@mui/material';
 import { useAppDispatch, useAppSelector } from '../../../app/hooks';
@@ -14,11 +12,8 @@ import ShipmentsCard from '../../shipments/components/ShipmentsCard';
 const UserOrdersTracking = () => {
   const dispatch = useAppDispatch();
   const order = useAppSelector(selectOneOrder);
-  const isMediumScreen = useMediaQuery('(max-width:1230px)');
   const isSmallScreen = useMediaQuery('(max-width:1000px)');
   const isExtraSmallScreen = useMediaQuery('(max-width:480px)');
-
-  const cards = Array.from({ length: 3 });
   const [state, setState] = useState<string>('');
   const [searched, setSearched] = useState<boolean>(false);
 
@@ -47,47 +42,6 @@ const UserOrdersTracking = () => {
           Поиск
         </Button>
       </Box>
-
-      {cards.map((_, index) => (
-        <Card sx={{ mt: 2, mb: 2 }} key={index}>
-          <CardContent>
-            <Grid container spacing={2}>
-              <Grid
-                container
-                spacing={ isExtraSmallScreen ? 2 : isMediumScreen ? 4 : 6 }
-                sx={{ ml: 1, borderBottom: '1px solid #000' }}
-              >
-                <Grid item>
-                  <Typography>Объем:</Typography>
-                </Grid>
-                <Grid item>
-                  <Typography>вес - 2кг,</Typography>
-                </Grid>
-                <Grid item>
-                  <Typography>ширина - 9м,</Typography>
-                </Grid>
-                <Grid item>
-                  <Typography>высота - 200м,</Typography>
-                </Grid>
-                <Grid item>
-                  <Typography>длинна - 2км</Typography>
-                </Grid>
-              </Grid>
-              <Grid item xs={7}>
-                <Typography gutterBottom>
-                  Трекинговый номер: sdlwpd209093so
-                </Typography>
-              </Grid>
-              <Grid item xs={7}>
-                <Typography gutterBottom>статус: КР_ПРИБЫЛО</Typography>
-              </Grid>
-              <Grid item xs={7}>
-                <Typography gutterBottom>цена: 200 сом</Typography>
-              </Grid>
-            </Grid>
-          </CardContent>
-        </Card>
-      ))}
       {order && <ShipmentsCard shipment={order} />}
       {searched && !order && (
         <Typography variant="body1" sx={{ mt: 2 }}>

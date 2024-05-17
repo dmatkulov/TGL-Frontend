b59924092f8fe164452001379c2c97c65a4957c7
Temirlan <temadjumanov@gmail.com>
2024-05-17 18:27:03 +0600

    TGL-140.feat: Updated PUP item: Added modal for delete, adapted component for a small devices

--- src/features/pups/components/PupItem.tsx
+++ src/features/pups/components/PupItem.tsx
@@ -23,6 +23,7 @@ import { LoadingButton } from '@mui/lab';
 import { isDeletePup } from '../pupsSlice';
 import { useNavigate } from 'react-router-dom';
 import { appRoutes } from '../../../utils/constants';
+import WarningPupModal from './WarningPupModal';
 
 interface Props {
   pupItem: Pup;
@@ -33,8 +34,9 @@ const PupItem: React.FC<Props> = ({ pupItem }) => {
   const user = useAppSelector(selectUser);
   const isDelete = useAppSelector(isDeletePup);
   const navigate = useNavigate();
-
   const [open, setOpen] = useState(false);
+  const [modalOpen, setModalOpen] = useState(false);
+
   const toggleOpen = () => {
     setOpen(true);
   };
@@ -48,6 +50,14 @@ const PupItem: React.FC<Props> = ({ pupItem }) => {
     ).unwrap();
     await dispatch(fetchPups());
     setOpen(false);
+  };
+
+  const openWarningModalWindow = () => {
+    setModalOpen(true);
+  };
+
+  const closeWarningModalWindow = () => {
+    setModalOpen(false);
   };
 
   const deleteHandler = async () => {
@@ -64,80 +74,106 @@ const PupItem: React.FC<Props> = ({ pupItem }) => {
   };
 
   return (
-    <Grid m={1}>
-      <Card>
-        <CardContent>
-          <Box
-            display="flex"
-            justifyContent="space-between"
-            alignItems="center"
-            mb={2}>
-            <Typography variant="h5" component="div">
-              {pupItem.name}
+    <>
+      <WarningPupModal
+        stateModal={modalOpen}
+        deleteHandler={deleteHandler}
+        closeModal={closeWarningModalWindow}
+      />
+
+      <Grid m={1}>
+        <Card>
+          <CardContent>
+            <Grid
+              container
+              justifyContent="space-between"
+              alignItems="center"
+              mb={2}
+            >
+              <Grid item>
+                <Typography variant="h5" component="div">
+                  {pupItem.name}
+                </Typography>
+              </Grid>
+              <Grid item xs={12} sm={5}>
+                <Box
+                  display="flex"
+                  gap={2}
+                  sx={{
+                    justifyContent: {
+                      xs: 'space-between',
+                      sm: 'flex-end',
+                    },
+                  }}
+                >
+                  {user && user?.role === 'super' && (
+                    <Button
+                      size="small"
+                      variant="contained"
+                      color="secondary"
+                      onClick={toggleOpen}
+                    >
+                      Редактировать
+                    </Button>
+                  )}
+                  {user && user?.role === 'super' && (
+                    <LoadingButton
+                      disabled={isDelete}
+                      loading={isDelete}
+                      onClick={openWarningModalWindow}
+                      sx={{
+                        minWidth: '29px',
+                        padding: '3px',
+                        borderRadius: '50%',
+                      }}
+                      color="error"
+                    >
+                      <CancelIcon />
+                    </LoadingButton>
+                  )}
+                </Box>
+              </Grid>
+            </Grid>
+            <Divider />
+            <Typography variant="body2" color="text.secondary">
+              <b>Адрес:</b> {pupItem.region.name} {pupItem.settlement}{' '}
+              {pupItem.address}
             </Typography>
-            <Box
-              display="flex"
-              gap={2}>
-              {user && user?.role === 'super' && (
-                <Button
-                  size="small"
-                  variant="contained"
-                  color="secondary"
-                  onClick={toggleOpen}>
-                  Редактировать
-                </Button>
-              )}
-              {user && user?.role === 'super' && (
-                <LoadingButton
-                  disabled={isDelete}
-                  loading={isDelete}
-                  onClick={deleteHandler}
-                  sx={{ minWidth: '29px', padding: '3px', borderRadius: '50%' }}
-                  color="error">
-                  <CancelIcon />
-                </LoadingButton>
-              )}
-            </Box>
-          </Box>
-          <Divider />
-          <Typography variant="body2" color="text.secondary">
-            <b>Адрес:</b> {pupItem.region.name} {pupItem.settlement}{' '}
-            {pupItem.address}
-          </Typography>
-          <Divider />
-          <Typography variant="body2" color="text.secondary">
-            <b>Тел:</b> +{pupItem.phoneNumber}
-          </Typography>
-        </CardContent>
-      </Card>
-      <Dialog open={open} onClose={handleClose} maxWidth="lg">
-        <DialogTitle>
-          <Typography>Добавить ПВЗ:</Typography>
-          <IconButton
-            onClick={handleClose}
+            <Divider />
+            <Typography variant="body2" color="text.secondary">
+              <b>Тел:</b> +{pupItem.phoneNumber}
+            </Typography>
+          </CardContent>
+        </Card>
+        <Dialog open={open} onClose={handleClose} maxWidth="lg">
+          <DialogTitle>
+            <Typography>Добавить ПВЗ:</Typography>
+            <IconButton
+              onClick={handleClose}
+              sx={{
+                position: 'absolute',
+                right: 8,
+                top: 8,
+                color: (theme) => theme.palette.grey[500],
+              }}
+            >
+              <CloseIcon />
+            </IconButton>
+          </DialogTitle>
+          <DialogContent
             sx={{
-              position: 'absolute',
-              right: 8,
-              top: 8,
-              color: (theme) => theme.palette.grey[500],
+              mt: '20px',
             }}
           >
-            <CloseIcon />
-          </IconButton>
-        </DialogTitle>
-        <DialogContent
-          sx={{
-            mt: '20px',
-          }}
-        >
-          <PupForm
-            onSubmit={submitFormHandler}
-            initialPupState={pupMutation}
-            isEdit
-          />
-        </DialogContent>
-      </Dialog>
-    </Grid>
+            <PupForm
+              onSubmit={submitFormHandler}
+              initialPupState={pupMutation}
+              isEdit
+            />
+          </DialogContent>
+        </Dialog>
+      </Grid>
+    </>
   );
 };
 

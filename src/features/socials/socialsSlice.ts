042413d32bb0b170a782a5a36fc17cff29e119dd
Alan Noruzbaev <alan.noruzbaev@gmail.com>
2024-05-19 01:53:44 +0600

    rw: rework slice logic

--- src/features/socials/socialsSlice.ts
+++ src/features/socials/socialsSlice.ts
@@ -1,33 +1,29 @@
 import { createSlice } from '@reduxjs/toolkit';
-import { Socials } from '../../types/types.SocialsNetwork';
-import {
-  createSocials,
-  deleteSocialNetwork,
-  fetchOneSocial,
-  fetchSocials,
-} from './socialsThunk';
+import { Social } from '../../types/types.SocialsNetwork';
+import { fetchSocials } from './socialsThunk';
 import { RootState } from '../../app/store';
 
 interface SocialsState {
-  socials: Socials[];
-  loadingSocials: boolean;
-  errorLoadingSocials: boolean;
-  isDelete: boolean;
-  social: Socials | null;
-  isLoadingDataSocial: boolean;
-  isErrorLoadDataSocial: boolean;
-  isEditing: boolean;
+  socials: Social[];
+  social: Social;
+  isLoading: boolean;
+  isSingleSocialLoading: boolean;
+  isUploading: boolean;
+  isDeleting: boolean;
 }
 
 const initialState: SocialsState = {
   socials: [],
-  loadingSocials: false,
-  errorLoadingSocials: false,
-  isDelete: false,
-  social: null,
-  isLoadingDataSocial: false,
-  isErrorLoadDataSocial: false,
-  isEditing: false,
+  social: {
+    _id: '',
+    name: '',
+    link: '',
+    image: '',
+  },
+  isLoading: false,
+  isSingleSocialLoading: false,
+  isUploading: false,
+  isDeleting: false,
 };
 
 export const socialsSlice = createSlice({
@@ -37,66 +33,64 @@ export const socialsSlice = createSlice({
   extraReducers: (builder) => {
     builder
       .addCase(fetchSocials.pending, (state) => {
-        state.loadingSocials = true;
-        state.errorLoadingSocials = false;
+        state.isLoading = true;
       })
       .addCase(fetchSocials.fulfilled, (state, { payload }) => {
-        state.loadingSocials = false;
+        state.isLoading = false;
         if (payload) {
           state.socials = payload.socials;
         }
       })
       .addCase(fetchSocials.rejected, (state) => {
-        state.loadingSocials = false;
-        state.errorLoadingSocials = true;
+        state.isLoading = false;
       });
 
-    builder
-      .addCase(fetchOneSocial.pending, (state) => {
-        state.isLoadingDataSocial = true;
-      })
-      .addCase(fetchOneSocial.fulfilled, (state, { payload: current }) => {
-        state.isLoadingDataSocial = false;
-        state.social = current;
-      })
-      .addCase(fetchOneSocial.rejected, (state) => {
-        state.isLoadingDataSocial = false;
-      });
-
-    builder
-      .addCase(createSocials.pending, (state) => {
-        state.isLoadingDataSocial = true;
-        state.isErrorLoadDataSocial = false;
-      })
-      .addCase(createSocials.fulfilled, (state) => {
-        state.isLoadingDataSocial = false;
-      })
-      .addCase(createSocials.rejected, (state) => {
-        state.isLoadingDataSocial = false;
-        state.isErrorLoadDataSocial = true;
-      });
-    builder
-      .addCase(deleteSocialNetwork.pending, (state) => {
-        state.isDelete = true;
-      })
-      .addCase(deleteSocialNetwork.fulfilled, (state) => {
-        state.isDelete = false;
-      })
-      .addCase(deleteSocialNetwork.rejected, (state) => {
-        state.isDelete = false;
-      });
+    // builder
+    //   .addCase(fetchOneSocial.pending, (state) => {
+    //     state.isLoadingDataSocial = true;
+    //   })
+    //   .addCase(fetchOneSocial.fulfilled, (state, { payload: current }) => {
+    //     state.isLoadingDataSocial = false;
+    //     state.social = current;
+    //   })
+    //   .addCase(fetchOneSocial.rejected, (state) => {
+    //     state.isLoadingDataSocial = false;
+    //   });
+    //
+    // builder
+    //   .addCase(createSocials.pending, (state) => {
+    //     state.isLoadingDataSocial = true;
+    //   })
+    //   .addCase(createSocials.fulfilled, (state) => {
+    //     state.isLoadingDataSocial = false;
+    //   })
+    //   .addCase(createSocials.rejected, (state) => {
+    //     state.isLoadingDataSocial = false;
+    //   });
+    // builder
+    //   .addCase(deleteSocialNetwork.pending, (state) => {
+    //     state.isDelete = true;
+    //   })
+    //   .addCase(deleteSocialNetwork.fulfilled, (state) => {
+    //     state.isDelete = false;
+    //   })
+    //   .addCase(deleteSocialNetwork.rejected, (state) => {
+    //     state.isDelete = false;
+    //   });
   },
 });
 
 export const socialReducer = socialsSlice.reducer;
-export const selectSocials = (state: RootState) => state.socials.socials;
-export const selectSocial = (state: RootState) => state.socials.social;
-export const getLoadingSocials = (state: RootState) =>
-  state.socials.loadingSocials;
-export const getErrorSocials = (state: RootState) =>
-  state.socials.loadingSocials;
-export const isPostLoadingSocials = (state: RootState) =>
-  state.socials.isLoadingDataSocial;
-export const isDeleteSocialNetwork = (state: RootState) =>
-  state.socials.isDelete;
-export const isEditing = (state: RootState) => state.socials.isEditing;
+export const socialsState = (state: RootState) => state.socials.socials;
+export const singleSocialState = (state: RootState) => state.socials.social;
+
+export const isSocialsLoading = (state: RootState) => state.socials.isLoading;
+export const isSingleSocialLoading = (state: RootState) =>
+  state.socials.isSingleSocialLoading;
+// export const selectSocials = (state: RootState) => state.socials.socials;
+// export const selectSocial = (state: RootState) => state.socials.social;
+// export const isPostLoadingSocials = (state: RootState) =>
+//   state.socials.isLoadingDataSocial;
+// export const isDeleteSocialNetwork = (state: RootState) =>
+//   state.socials.isDelete;
+// export const isEditing = (state: RootState) => state.socials.isEditing;

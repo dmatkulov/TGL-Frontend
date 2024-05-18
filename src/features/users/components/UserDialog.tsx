e839d064d89bb20115db266c196ba7eb99011b15
Sergeev Mikhail <sergeev-michael@mail.ru>
2024-05-18 12:21:07 +0600

    "TGL-138(fix): Do refactor code on component Profile.tsx, UserDialog.tsx"

--- src/features/users/components/UserDialog.tsx
+++ src/features/users/components/UserDialog.tsx
@@ -1,17 +1,10 @@
-import {
-  Button,
-  Dialog,
-  DialogContent,
-  DialogTitle,
-  Grid,
-  MenuItem,
-  TextField,
-} from '@mui/material';
 import React, { useEffect } from 'react';
-import { ProfileMutation } from '../../../types/types.Profile';
-import { useAppDispatch, useAppSelector } from '../../../app/hooks';
+import { Button, Dialog, DialogContent, DialogTitle, Grid, MenuItem, TextField } from '@mui/material';
+
 import { regionsState } from '../../regions/regionsSlice';
 import { fetchRegions } from '../../regions/regionsThunks';
+import { useAppDispatch, useAppSelector } from '../../../app/hooks';
+import { ProfileMutation } from '../../../types/types.Profile';
 
 interface Props {
   state: ProfileMutation;
@@ -40,25 +33,17 @@ const UserDialog: React.FC<Props> = ({
       <Dialog open={open} onClose={handleClose} maxWidth="lg">
         <DialogTitle>Редактирование профиля</DialogTitle>
         <DialogContent
-          sx={{
-            mt: '20px',
-          }}
+          sx={{mt: '20px'}}
         >
           <form autoComplete="off">
-            {/*onsubmit */}
             <Button
-              sx={{
-                '&.MuiButton-root:hover': { background: '#D2122E' },
+              sx={{'&.MuiButton-root:hover': { background: '#D2122E' },
                 color: 'white',
                 background: '#9e1b32',
                 position: 'absolute',
                 top: 0,
-                right: 0,
-              }}
-              onClick={handleClose}
-            >
-              X
-            </Button>
+                right: 0}}
+              onClick={handleClose}>X</Button>
             <Grid container direction="column" spacing={2}>
               <Grid item xs={12} container gap={'10px'} sx={{ mt: 1 }}>
                 <TextField
@@ -139,8 +124,7 @@ const UserDialog: React.FC<Props> = ({
                   type="submit"
                   color="primary"
                   variant="contained"
-                  onClick={handleUpdateProfile}
-                >
+                  onClick={handleUpdateProfile}>
                   Редактировать данные
                 </Button>
               </Grid>
--- src/features/users/containers/Profile.tsx
+++ src/features/users/containers/Profile.tsx
@@ -1,16 +1,21 @@
-import { Button, Grid, Typography } from '@mui/material';
 import React, { useState } from 'react';
+import { Button, Grid, Typography } from '@mui/material';
+
 import { useAppDispatch, useAppSelector } from '../../../app/hooks';
 import { selectUser } from '../usersSlice';
-import { ProfileMutation } from '../../../types/types.Profile';
-import Warehouses from '../../warehouses/Warehouses';
 import { update } from '../usersThunks';
+
+import Warehouses from '../../warehouses/Warehouses';
 import UserDialog from '../components/UserDialog';
+import { ProfileMutation } from '../../../types/types.Profile';
 import BorderColorIcon from '@mui/icons-material/BorderColor';
+
 const Profile = () => {
+
   const dispatch = useAppDispatch();
   const user = useAppSelector(selectUser);
   const [open, setOpen] = useState(false);
+
   const [state, setState] = useState<ProfileMutation>({
     email: user?.email,
     firstName: user?.firstName,
@@ -20,6 +25,7 @@ const Profile = () => {
     settlement: user?.settlement,
     address: user?.address,
   });
+
   const isAdmin = user?.role === 'super' || user?.role === 'admin';
   const handleClickOpen = () => {
     setOpen(true);
@@ -31,9 +37,7 @@ const Profile = () => {
 
   const handleUpdateProfile = async (e: React.FormEvent) => {
     e.preventDefault();
-
     await dispatch(update(state));
-
     setOpen(false);
   };
 
@@ -52,13 +56,10 @@ const Profile = () => {
               gutterBottom
               variant="h6"
               component="h1"
-              fontWeight="bold"
-            >
+              fontWeight="bold">
               {user?.firstName} {user?.lastName}
             </Typography>
-            {isAdmin ? (
-              <></>
-            ) : (
+            {isAdmin ? (<></>) : (
               <>
                 <Typography variant="subtitle1">
                   Ваш персональный код: {user?.marketId}
@@ -68,8 +69,8 @@ const Profile = () => {
                   {user?.address}
                 </Typography>
                 <Typography variant="subtitle1">
-                  Ваш {user?.pupID.name} находится по адресу: {user?.pupID.address}
-
+                  Ваш {user?.pupID.name} находится по адресу: {' '}
+                  {user?.pupID.address}
                 </Typography>
               </>
             )}
@@ -79,21 +80,17 @@ const Profile = () => {
               startIcon={<BorderColorIcon />}
               onClick={handleClickOpen}
               color="secondary"
-              sx={{ textTransform: 'none' }}
-            >
+              sx={{ textTransform: 'none' }}>
               Редактировать профиль
             </Button>
           </Grid>
         </Grid>
-        {isAdmin ? (
-          <></>
-        ) : (
+        {isAdmin ? (<></>) : (
           <Grid item>
             <Warehouses />
           </Grid>
         )}
       </Grid>
-
       <UserDialog
         state={state}
         open={open}

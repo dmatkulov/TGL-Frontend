720c5efb1191c8fdb44c1b4d2908677da0934688
zanylbajguttieva <janabaiguttieva@gmail.com>
2024-06-01 16:29:36 +0600

    Added datetime and pagination in table of orders, installed dayjs

--- src/features/orders/components/OrdersHistoryItem.tsx
+++ src/features/orders/components/OrdersHistoryItem.tsx
@@ -1,15 +1,20 @@
 import { FC } from 'react';
 import { TableCell, TableRow } from '@mui/material';
 import { ShipmentThatDone } from '../../../types/types.Shipments';
+import dayjs from 'dayjs';
 
 const OrdersHistoryItem: FC<ShipmentThatDone> = ({
   pupId,
   price,
   trackerNumber,
+  datetime,
 }) => {
+
+  const formattedDate = dayjs(datetime).format('YYYY-MM-DD HH:mm:ss');
   return (
     <>
       <TableRow sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
+        <TableCell>{formattedDate}</TableCell>
         <TableCell component="th" scope="row">
           {trackerNumber}
         </TableCell>

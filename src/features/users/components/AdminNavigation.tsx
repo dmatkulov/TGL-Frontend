23db8aed5ec40989f75a54f27f04b9f625c5ba10
Dilshad Matkulov <dilshadmatkulov@gmail.com>
2024-06-23 15:29:19 +0600

    TGL-226.feat: changed manager navigation

--- src/features/users/components/AdminNavigation.tsx
+++ src/features/users/components/AdminNavigation.tsx
@@ -21,6 +21,8 @@ import CurrencyExchangeIcon from '@mui/icons-material/CurrencyExchange';
 import RecommendIcon from '@mui/icons-material/Recommend';
 import GroupsIcon from '@mui/icons-material/Groups';
 import { Block } from '@mui/icons-material';
+import { useAppSelector } from '../../../app/hooks';
+import { selectUser } from '../usersSlice';
 
 const adminLinks: UserNav[] = [
   {
@@ -103,54 +105,143 @@ const adminLinks: UserNav[] = [
   },
 ];
 
+const managerLinks: UserNav[] = [
+  {
+    id: 1,
+    name: 'Мой профиль',
+    navLink: appRoutes.myAdminProfile,
+    icon: <AnalyticsIcon color="primary" />,
+  },
+  {
+    id: 2,
+    name: 'Статистика',
+    navLink: appRoutes.statistics,
+    icon: <AnalyticsIcon color="primary" />,
+  },
+  {
+    id: 3,
+    name: 'ПВЗ',
+    navLink: appRoutes.pups,
+    icon: <StoreIcon color="primary" />,
+  },
+  {
+    id: 4,
+    name: 'Сотрудники',
+    navLink: appRoutes.staff,
+    icon: <PeopleIcon color="primary" />,
+  },
+  {
+    id: 5,
+    name: 'Заказы клиентов',
+    navLink: appRoutes.shipments,
+    icon: <LocalGroceryStoreIcon color="primary" />,
+  },
+  {
+    id: 7,
+    name: 'Склад в Китае',
+    navLink: appRoutes.adminWarehouses,
+    icon: <WarehouseIcon color="primary" />,
+  },
+  {
+    id: 8,
+    name: 'Физический адрес',
+    navLink: appRoutes.adminCompanyAddress,
+    icon: <HomeIcon color="primary" />,
+  },
+  {
+    id: 9,
+    name: 'Цена',
+    navLink: appRoutes.price,
+    icon: <CurrencyExchangeIcon color="primary" />,
+  },
+  {
+    id: 10,
+    name: 'Прайс-лист',
+    navLink: appRoutes.priceLists,
+    icon: <CurrencyExchangeIcon color="primary" />,
+  },
+  {
+    id: 11,
+    name: 'Социальные сети',
+    navLink: appRoutes.socials,
+    icon: <RecommendIcon color="primary" />,
+  },
+  {
+    id: 12,
+    name: 'Клиенты',
+    navLink: appRoutes.adminClients,
+    icon: <GroupsIcon color="primary" />,
+  },
+  {
+    id: 13,
+    name: 'Запрет',
+    navLink: appRoutes.adminBanned,
+    icon: <Block color="primary" />,
+  },
+];
+
 const AdminNavigation = () => {
   const isSmallScreen = useMediaQuery('(max-width:850px)');
   const navigate = useNavigate();
   const activePath = location.pathname;
 
+  const user = useAppSelector(selectUser);
+
+  let navigation;
+
+  if (user) {
+    if (user.role === 'manager') {
+      navigation = managerLinks;
+    } else {
+      navigation = adminLinks;
+    }
+  }
+
   return (
-    <>
-      <Box
-        sx={{
-          bgcolor: 'background.paper',
-          borderColor: 'grey.400',
-          borderRadius: '16px',
-          borderWidth: '1px',
-        }}
-      >
-        <nav>
-          <List
-            dense
-            sx={{
-              display: isSmallScreen ? 'flex' : '',
-              flexWrap: isSmallScreen ? 'wrap' : '',
-            }}
-          >
-            {adminLinks.map((link) => (
-              <ListItem key={link.id} disableGutters>
-                <ListItemButton
-                  selected={activePath === link.navLink}
-                  onClick={() => {
-                    navigate(link.navLink);
-                  }}
-                  sx={{ borderRadius: 2 }}
-                >
-                  <ListItemIcon>{link.icon}</ListItemIcon>
-                  <ListItemText
-                    primary={link.name}
-                    primaryTypographyProps={{
-                      fontSize: 16,
-                      color:
-                        activePath === link.navLink ? 'primary' : 'inherit',
+    navigation && (
+      <>
+        <Box
+          sx={{
+            bgcolor: 'background.paper',
+            borderColor: 'grey.400',
+            borderRadius: '16px',
+            borderWidth: '1px',
+          }}
+        >
+          <nav>
+            <List
+              dense
+              sx={{
+                display: isSmallScreen ? 'flex' : '',
+                flexWrap: isSmallScreen ? 'wrap' : '',
+              }}
+            >
+              {navigation.map((link) => (
+                <ListItem key={link.id} disableGutters>
+                  <ListItemButton
+                    selected={activePath === link.navLink}
+                    onClick={() => {
+                      navigate(link.navLink);
                     }}
-                  />
-                </ListItemButton>
-              </ListItem>
-            ))}
-          </List>
-        </nav>
-      </Box>
-    </>
+                    sx={{ borderRadius: 2 }}
+                  >
+                    <ListItemIcon>{link.icon}</ListItemIcon>
+                    <ListItemText
+                      primary={link.name}
+                      primaryTypographyProps={{
+                        fontSize: 16,
+                        color:
+                          activePath === link.navLink ? 'primary' : 'inherit',
+                      }}
+                    />
+                  </ListItemButton>
+                </ListItem>
+              ))}
+            </List>
+          </nav>
+        </Box>
+      </>
+    )
   );
 };
 
